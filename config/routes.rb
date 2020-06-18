e5e83c0ca2e5a133f044ece837b4d76650ba9bf4
Simon Leiva <leivaprogramming@gmail.com>
2020-06-18 17:50:56 -0400

    did all the seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,30 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'books#index'
 
   resources :books, only: [ :index, :show ] do
-    collection do
-      get 'sociology' => 'books#sociology'
-    end
+    # collection do
+    #   get 'sociology' => 'books#sociology'
+    # end
 
-    collection do
-      get 'history' => 'books#history'
-    end
+    # collection do
+    #   get 'history' => 'books#history'
+    # end
 
-    collection do
-      get 'economy' => 'books#economy'
-    end
+    # collection do
+    #   get 'economy' => 'books#economy'
+    # end
 
-    collection do
-      get 'business' => 'books#business'
-    end
+    # collection do
+    #   get 'business' => 'books#business'
+    # end
 
-    collection do
-      get 'investing' => 'books#investing'
-    end
+    # collection do
+    #   get 'investing' => 'books#investing'
+    # end
 
-    collection do
-      get 'trading' => 'books#trading'
-    end
+    # collection do
+    #   get 'trading' => 'books#trading'
+    # end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
